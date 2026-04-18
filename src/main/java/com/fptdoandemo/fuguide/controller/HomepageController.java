7988eea4be83d76c8a308cb6a1cc294b9592f314
Tunkyo <vuquangtung2004@gmail.com>
2026-04-19 01:20:13 +0700

    feat: Implement homepage with program and news listings

--- src/main/java/com/fptdoandemo/fuguide/controller/HomepageController.java
+++ src/main/java/com/fptdoandemo/fuguide/controller/HomepageController.java
@@ -1,12 +1,26 @@
 package com.fptdoandemo.fuguide.controller;
 
+import com.fptdoandemo.fuguide.repository.NewsRepository;
+import com.fptdoandemo.fuguide.repository.ProgramRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomepageController {
+    @Autowired
+    private ProgramRepository programRepo;
+
+    @Autowired
+    private NewsRepository newsRepo;
+
     @GetMapping("/")
-    public String hello() {
-        return "Hello World!";
+    public String index(Model model) {
+        model.addAttribute("programs", programRepo.findAll());
+        model.addAttribute("news", newsRepo.findAll());
+
+        return "index";
     }
 }
