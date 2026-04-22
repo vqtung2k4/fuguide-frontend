948cf9be99b0c62ad11336276a56f70afa934baf
minhvbhe187102 <minhvbhe187102@fpt.edu.vn>
2026-04-22 16:05:02 +0700

    firebase
    now use firebase instead of local db

--- src/main/java/com/fptdoandemo/fuguide/model/CampusLife.java
+++ src/main/java/com/fptdoandemo/fuguide/model/CampusLife.java
@@ -1,21 +1,14 @@
 package com.fptdoandemo.fuguide.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CampusLife {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
 
     private String title;
     private String description;
