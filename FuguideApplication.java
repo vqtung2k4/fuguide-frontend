eae93e424bfbe16755687edd231feab73a28d63d
minhvbhe187102 <minhvbhe187102@fpt.edu.vn>
2026-04-22 19:44:00 +0700

    remove mock data

---
+++ FuguideApplication.java
@@ -0,0 +1,12 @@
+package com.fptdoandemo.fuguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuguideApplication.class, args);
+	}
+}
